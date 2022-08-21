51c3a52bb7ffabe5b1c50884196a5131b944124d
Pavel <pavel.ancl@gmail.com>
2022-08-21 18:37:22 +0300

    - load game - load ssh - fix controller

--- 11.py
+++ 11.py
@@ -1,33 +1,63 @@
 import os
+from datetime import datetime
 from multiprocessing import Process
 from time import sleep
-
-import keyboard
 import pyautogui as pag
-from pygetwindow import getWindowsWithTitle, getAllTitles
 
 
-def func2():
+path = "C:/Users/Katerina/PycharmProjects/Saber_Interactive_test_task/Saber_Interactive_test_task"
+
+
+def push_the_button(button_name, hold=0.1):
+    pag.keyDown(button_name)
+    sleep(hold)
+    pag.keyUp(button_name)
+    # sleep(hold)
+
+
+def run_game():
     os.system("C:/Users/Katerina/Desktop/pno0001.exe")
 
 
-def func1():
+def actions():
     print("Функция У ожидает")
-    sleep(45)
+    sleep(40)
     print("Функция У в работе")
 
-    pag.keyDown('esc')
-    sleep(0.1)
-    pag.keyUp('esc')
-    sleep(2)
-    pag.keyDown('enter')
-    sleep(0.1)
-    pag.keyUp('enter')
+    push_the_button('esc')
+    # sleep(2)
+    push_the_button('enter')
+
+    push_the_button('8')
+
+    sleep(1)
+    pag.screenshot(f"{path}/{datetime.now().strftime('%H-%M-%S')}.jpg")
+
+    push_the_button('w', 5)
+
+    pag.screenshot(f"{path}/{datetime.now().strftime('%H-%M-%S')}.jpg")
+
+    push_the_button('8')
+
+    push_the_button('esc')
+
+    push_the_button('down')
+
+    push_the_button('down')
+
+    push_the_button('enter')
+
+    push_the_button('down')
+
+    push_the_button('down')
+
+    push_the_button('enter')
 
 
 if __name__ == '__main__':
-     p1 = Process(target=func1)
-     p1.start()
-     p2 = Process(target=func2)
-     p2.start()
+    p1 = Process(target=actions)
+    p1.start()
+    p2 = Process(target=run_game)
+    p2.start()
+
 
