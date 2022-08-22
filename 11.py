5e4133a8e02fad731a6aedc3d6725fffc541deb6
Pavel <pavel.ancl@gmail.com>
2022-08-22 22:49:25 +0300

    - run because of a pixel's color

--- 11.py
+++ 11.py
@@ -4,35 +4,53 @@ from multiprocessing import Process
 from time import sleep
 import pyautogui as pag
 
+# pag.pixelMatchesColor()
 
 path = "C:/Users/Katerina/PycharmProjects/Saber_Interactive_test_task/Saber_Interactive_test_task"
+path_to_run_file = "C:/Users/Katerina/Desktop/pno0001.exe"
 
 
 def push_the_button(button_name, hold=0.1):
     pag.keyDown(button_name)
     sleep(hold)
     pag.keyUp(button_name)
-    # sleep(hold)
+    sleep(hold)
 
 
 def run_game():
-    os.system("C:/Users/Katerina/Desktop/pno0001.exe")
+    os.system(path_to_run_file)
+
+
+def wait():
+    sleep(5)
+    # size = pag.size()
+    pag.moveTo(600, 500)
+    start_color = pag.pixel(600, 500)
+    print(f'start_color - {start_color}')
+    while pag.pixel(600, 500) == start_color:
+        print(f'actual_color - {pag.pixel(600, 500)}')
+        sleep(1)
+    print('end')
 
 
 def actions():
-    print("Функция У ожидает")
-    sleep(40)
-    print("Функция У в работе")
+    # print("Функция ожидания работает")
+    # sleep(45)
+    #
+    # print("Функция ожидания дождалась")
 
+    wait()
     push_the_button('esc')
-    # sleep(2)
+    sleep(2)
     push_the_button('enter')
+
+    print(pag.size())
 
     push_the_button('8')
 
     sleep(1)
     pag.screenshot(f"{path}/{datetime.now().strftime('%H-%M-%S')}.jpg")
-
+    sleep(1)
     push_the_button('w', 5)
 
     pag.screenshot(f"{path}/{datetime.now().strftime('%H-%M-%S')}.jpg")
@@ -55,9 +73,12 @@ def actions():
 
 
 if __name__ == '__main__':
-    p1 = Process(target=actions)
+    # run_game()
+    # wait()
+    # actions()
+    p1 = Process(target=run_game)
     p1.start()
-    p2 = Process(target=run_game)
+    # pw = Process(target=wait)
+    # pw.start()
+    p2 = Process(target=actions)
     p2.start()
-
-
